8ee09192e6722eb368877ea4e21f9d3dc75df1b5
WangZhenghai <fisschl@163.com>
2025-08-01 11:54:14 +0800

    移除 Noto Sans SC 字体依赖，新增 MiSans 字体及其样式文件；更新主样式以适应新字体，优化首页样式布局。

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@ import { ElConfigProvider } from "element-plus";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import { useAutoDarkMode } from "./utils/dark";
 import "@fontsource-variable/fira-code";
-import "@fontsource-variable/noto-sans-sc";
 import "dayjs/locale/zh-cn";
 import "element-plus/theme-chalk/dark/css-vars.css";
+import "./assets/MiSans/mi-sans.css";
+import "./assets/reset.css";
 
 useAutoDarkMode();
 </script>
